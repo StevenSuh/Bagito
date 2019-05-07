4e0fc107852736447260009d5f08fcfff6d24953
Steven Suh <stevenesuh@gmail.com>
2019-05-06 18:46:27 -0700

    add navbar and actionbar

--- app/app/src/main/java/com/example/bagito/MainActivity.java
+++ app/app/src/main/java/com/example/bagito/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.bagito;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 
@@ -25,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setActionBarColor();
+
         Button mLogoutButton = findViewById(R.id.logout_button);
         mLogoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +41,15 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void setActionBarColor() {
+        ActionBar actionBar = getSupportActionBar();
+        System.out.println(actionBar);
+
+        if (actionBar != null) {
+            actionBar.setTitle(Html.fromHtml("<b><font color=\"" + getResources().getColor(R.color.colorPrimary) + "\">" + getString(R.string.app_name) + "</font></b>"));
+            actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorLight)));
+        }
     }
 }
