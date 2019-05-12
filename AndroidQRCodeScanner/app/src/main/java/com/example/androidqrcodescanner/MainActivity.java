cedf4081846eff3053321860137ecec276cdfa47
Matthew <mngor@ucsc.edu>
2019-05-11 20:12:28 -0700

    Added functionality; onCreate() method

--- AndroidQRCodeScanner/app/src/main/java/com/example/androidqrcodescanner/MainActivity.java
+++ AndroidQRCodeScanner/app/src/main/java/com/example/androidqrcodescanner/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.androidqrcodescanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+// additional imports from androidtutorialonline.com
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.Result;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import static android.Manifest.permission.CAMERA;
+
+// implementation also acts as Barcode scanner
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private static final int REQUEST_CAMERA = 1;
+    private ZXingScannerView mScannerView;          // view to scan the QR code
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        //setContentView(R.layout.activity_main); //default setContentView
+        Log.e("onCreate", "onCreate");
+
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        // Have to get user permissions at runtime --> check API version
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+        if (currentapiVersion >= android.os.Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                Toast.makeText(getApplicationContext(),
+                        "Permission already granted",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                requestPermission();
+            }
+        }
+    }<code class="java plain">
+
 }
