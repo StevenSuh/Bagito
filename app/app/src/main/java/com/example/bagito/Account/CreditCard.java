4e0088429b9c67552695a54fc68f208b52195401
Wilson Chiu <wilsonmchiu@gmail.com>
2019-05-21 18:54:55 -0700

    started change settings

---
+++ app/app/src/main/java/com/example/bagito/Account/CreditCard.java
@@ -0,0 +1,15 @@
+package com.example.bagito.Account;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.bagito.R;
+
+public class CreditCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card);
+    }
+}
