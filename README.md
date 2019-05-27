197feb56f3821185f34750e0256af4b3c078d89b
Steven Suh <stevenesuh@gmail.com>
2019-05-26 21:47:03 -0700

    complete google cloud deployment

--- README.md
+++ README.md
@@ -8,5 +8,4 @@ Note: `/server/credentials.json` is a combination of keys from google API, mailj
 - Get mailjet API keys from [mailjet API keys](https://app.mailjet.com/account/api_keys). Add the keys as `MJ_APIKEY_PUBLIC` and `MJ_APIKEY_PRIVATE`.
 - Get stripe API key from [stripe dashboard](https://dashboard.stripe.com/test/apikeys). Add the secret key as `STRIPE_APIKEY`.
 - Place the file in `/server`.
-<br>
 - Apart from `credentials.json`, modify the `stripe_key` XML value in `/app/BagitoApp/java/res/values/string.xml` to the stripe public API key.
--- server/config.py
+++ server/config.py
@@ -1,7 +1,7 @@
 # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<INSTANCE_NAME>
-# to access Google Cloud SQL locally 'mysql+pymysql://root:power2sustain@/bagito?127.0.0.1:3306'
+# to access Google Cloud SQL locally, replace with'mysql+pymysql://root:power2sustain@/bagito?127.0.0.1:3306' after setting up cloud sql proxy
 # localhost 'sqlite:///test.db'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<db_user>:<db_password>@/<db_name>?unix_socket=/cloudsql/bagito:us-west1:bagito-database'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/bagito:us-central1:bagito-db'
 SQLALCHEMY_POOL_RECYCLE = 3600
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
